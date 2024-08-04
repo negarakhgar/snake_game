a41774253bb3466747f3252b41ddd7a9421f12b8
negar <negar.akhgar1375@gmail.com>
2024-08-04 19:15:41 +0330

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,55 +1,52 @@
-from turtle import Screen, Turtle
-import time
-from snake import Snake
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title("my snake game!")
-screen.tracer(0)# Turn off automatic updates
-
-snake = Snake()
-
-screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
-game_is_on = True
-while game_is_on:
-    screen.update() # Refresh the screen to show the movement
-    time.sleep(1)
-    snake.move()
+class Snake:
 
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
 
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
-screen.exitonclick()
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
-#segment_2 = Turtle("square")
-#segment_2.color("white")
-#segment_2.goto(-20,0)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
-
-#segment_3 = Turtle("square")
-#segment_3.color("white")
-#segment_3.goto(-40,0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
